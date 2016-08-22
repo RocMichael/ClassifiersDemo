d6098fcb5c5de96c5229bc5d52619a415e474d4f
HDT3213 <hdt3213@outlook.com>
2016-08-22 21:46:06 +0800

    remove useful method

--- KMean/KMean.py
+++ KMean/KMean.py
@@ -1,16 +1,6 @@
 from numpy import *
 
 
-def loadDataSet(fileName):  # general function to parse tab -delimited floats
-    dataMat = []  # assume last column is target value
-    fr = open(fileName)
-    for line in fr.readlines():
-        curLine = line.strip().split('\t')
-        fltLine = map(float, curLine)  # map all elements to float()
-        dataMat.append(fltLine)
-    return dataMat
-
-
 def distEclud(vecA, vecB):
     return sqrt(sum(power(vecA - vecB, 2)))
 
