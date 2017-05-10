3e8c6bad9a79c1284d12a0664e58127bab3a4609
hdt3213 <hdt3213@outlook.com>
2017-05-10 16:00:11 +0800

    working on lstm

--- RNN/f.py
+++ RNN/f.py
@@ -55,20 +55,75 @@ class LSTM:
         self.output_weights = make_rand_mat(self.hidden_n, self.output_n)
         self.hidden_weights = make_rand_mat(self.hidden_n, self.hidden_n)
 
-    def train(self):
-        pass
-
     def predict(self, inputs):
         pass
 
-    def train(self, cases=None, labels=None, dim=0, learn=0.1, limit=20000):
+    def train(self, case=None, label=None, dim=0, learn=0.1):
         input_updates = np.zeros_like(self.input_weights)
         output_updates = np.zeros_like(self.output_weights)
         hidden_updates = np.zeros_like(self.hidden_weights)
 
-        # training logic
-        for j in range(limit):
+        a = case[0]
+        b = case[1]
+        c = label
 
+        d = np.zeros_like(c)
+
+        error = 0
+
+        layer_2_deltas = list()
+        layer_1_values = list()
+        layer_1_values.append(np.zeros(self.hidden_n))
+
+        # moving along the positions in the binary encoding
+        for position in range(dim):
+            # generate input and output
+            X = np.array([[a[dim - position - 1], b[dim - position - 1]]])
+            y = np.array([[c[dim - position - 1]]]).T
+
+            # hidden layer (input ~+ prev_hidden)
+            layer_1 = sigmoid(np.dot(X, self.input_weights) + np.dot(layer_1_values[-1], self.hidden_weights))
+
+            # output layer (new binary representation)
+            layer_2 = sigmoid(np.dot(layer_1, self.output_weights))
+
+            # did we miss?... if so by how much?
+            layer_2_error = y - layer_2
+            layer_2_deltas.append((layer_2_error) * sigmoid_derivative(layer_2))
+            error += np.abs(layer_2_error[0])
+
+            d[dim - position - 1] = np.round(layer_2[0][0])
+
+            layer_1_values.append(copy.deepcopy(layer_1))
+
+        future_layer_1_delta = np.zeros(self.hidden_n)
+
+        for position in range(dim):
+            X = np.array([[a[position], b[position]]])
+            layer_1 = layer_1_values[-position - 1]
+            prev_layer_1 = layer_1_values[-position - 2]
+
+            # error at output layer
+            layer_2_delta = layer_2_deltas[-position - 1]
+            # error at hidden layer
+            layer_1_delta = (future_layer_1_delta.dot(self.hidden_weights.T) +
+                             layer_2_delta.dot(self.output_weights.T)) * sigmoid_derivative(layer_1)
+            # let's update all our weights so we can try again
+            output_updates += np.atleast_2d(layer_1).T.dot(layer_2_delta)
+            hidden_updates += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
+            input_updates += X.T.dot(layer_1_delta)
+
+            future_layer_1_delta = layer_1_delta
+
+        self.input_weights += input_updates * learn
+        self.output_weights += output_updates * learn
+        self.hidden_weights += hidden_updates * learn
+
+        return d, error
+
+    def test(self):
+        self.setup(2, 16, 1)
+        for i in range(20000):
             # generate a simple addition problem (a + b = c)
             a_int = int(rand(0, 127))
             a = int_to_bin(a_int, dim=8)
@@ -83,66 +138,10 @@ class LSTM:
             c = int_to_bin(c_int, dim=8)
             c = np.array([int(t) for t in c])
 
-            # where we'll store our best guess (binary encoded)
-            d = np.zeros_like(c)
+            d, error = self.train([a, b], c, dim=8)
 
-            overallError = 0
-
-            layer_2_deltas = list()
-            layer_1_values = list()
-            layer_1_values.append(np.zeros(self.hidden_n))
-
-            # moving along the positions in the binary encoding
-            for position in range(dim):
-                # generate input and output
-                X = np.array([[a[dim - position - 1], b[dim - position - 1]]])
-                y = np.array([[c[dim - position - 1]]]).T
-
-                # hidden layer (input ~+ prev_hidden)
-                layer_1 = sigmoid(np.dot(X, self.input_weights) + np.dot(layer_1_values[-1], self.hidden_weights))
-
-                # output layer (new binary representation)
-                layer_2 = sigmoid(np.dot(layer_1, self.output_weights))
-
-                # did we miss?... if so by how much?
-                layer_2_error = y - layer_2
-                layer_2_deltas.append((layer_2_error) * sigmoid_derivative(layer_2))
-                overallError += np.abs(layer_2_error[0])
-
-                d[dim - position - 1] = np.round(layer_2[0][0])
-
-                layer_1_values.append(copy.deepcopy(layer_1))
-
-            future_layer_1_delta = np.zeros(self.hidden_n)
-
-            for position in range(dim):
-                X = np.array([[a[position], b[position]]])
-                layer_1 = layer_1_values[-position - 1]
-                prev_layer_1 = layer_1_values[-position - 2]
-
-                # error at output layer
-                layer_2_delta = layer_2_deltas[-position - 1]
-                # error at hidden layer
-                layer_1_delta = (future_layer_1_delta.dot(self.hidden_weights.T) +
-                                 layer_2_delta.dot(self.output_weights.T)) * sigmoid_derivative(layer_1)
-                # let's update all our weights so we can try again
-                output_updates += np.atleast_2d(layer_1).T.dot(layer_2_delta)
-                hidden_updates += np.atleast_2d(prev_layer_1).T.dot(layer_1_delta)
-                input_updates += X.T.dot(layer_1_delta)
-
-                future_layer_1_delta = layer_1_delta
-
-            self.input_weights += input_updates * learn
-            self.output_weights += output_updates * learn
-            self.hidden_weights += hidden_updates * learn
-
-            input_updates *= 0
-            output_updates *= 0
-            hidden_updates *= 0
-
-            # print out progress
-            if j % 1000 == 0:
-                print "Error:" + str(overallError)
+            if i % 1000 == 0:
+                print "Error:" + str(error)
                 print "Pred:" + str(d)
                 print "True:" + str(c)
                 out = 0
@@ -150,11 +149,6 @@ class LSTM:
                     out += x * pow(2, index)
                 print str(a_int) + " + " + str(b_int) + " = " + str(out)
                 print "------------"
-
-    def test(self):
-        self.setup(2, 16, 1)
-        self.train(dim=8)
-
 
 if __name__ == '__main__':
     nn = LSTM()
